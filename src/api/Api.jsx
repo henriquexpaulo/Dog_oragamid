b2467b21327aae17a2f01168c923ba341cd62e45
henriquexpaulo <phvieira3@gmail.com>
2024-09-19 16:46:17 -0300

    first commit

---
+++ src/api/Api.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import UserPost from './endpoints/UserPost'
+import TokenPost from './endpoints/TokenPost'
+import PhotoPost from './endpoints/PhotoPost'
+
+const Api = () => {
+  return <div> 
+    <h2>USER POST</h2>
+    <UserPost />
+    <h2>Token post</h2>
+    <TokenPost />
+    <h2>PHOTO POST</h2>
+    <PhotoPost />
+  </div>
+}
+
+export default Api
